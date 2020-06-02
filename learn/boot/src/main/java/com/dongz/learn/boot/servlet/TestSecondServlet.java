8f38b82f171548b9e9c2e45aef2b86942d330a39
dong <123@qq.com>
2020-06-02 11:31:10 +0800

    手写spring boot

---
+++ learn/boot/src/main/java/com/dongz/learn/boot/servlet/TestSecondServlet.java
@@ -0,0 +1,16 @@
+package com.dongz.learn.boot.servlet;
+
+import javax.servlet.ServletContainerInitializer;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
+import java.util.Set;
+
+public class TestSecondServlet implements ServletContainerInitializer {
+    @Override
+    public void onStartup(Set<Class<?>> set, ServletContext servletContext) throws ServletException {
+        System.out.println("------------");
+        ServletRegistration.Dynamic servlet = servletContext.addServlet("index", new SecondServlet());
+        servlet.addMapping("/index");
+    }
+}
